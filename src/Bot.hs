65bae3d7eaefb0ebc2399507410fac198081e5f4
Gleb Akhmerov <g.akhmerov@gmail.com>
2020-08-05 22:36:18 +0400

    Add /repeat command (closes #4)

--- src/Bot.hs
+++ src/Bot.hs
@@ -11,30 +11,52 @@ module Bot
     ) where
 
 
-data Config = Config { repeats :: Int, helpText :: String }
+data Config = Config { repeats :: Int
+                     , helpText :: String
+                     , repeatKeyboardText :: String
+                     } deriving (Show, Eq)
 
 defaultConfig =
   Config
     { repeats = 1
     , helpText = "This bot repeats the messages that you send it."
+    , repeatKeyboardText = "How many times would you want me to repeat my messages?"
     }
 
-data InMessage = InTextMessage String deriving (Show, Eq)
+data InMessage
+  = InTextMessage String
+  | KeyboardKeyPushed Int
+  deriving (Show, Eq)
 
-data OutMessage = OutTextMessage String deriving (Show, Eq)
+data OutMessage
+  = OutTextMessage String
+  | SendKeyboard String [Int]
+  deriving (Show, Eq)
 
 data Action
   = Echo String
   | Help
+  | RequestRepeatKeyboard
+  | SetRepeatTo Int
   deriving (Show, Eq)
 
 messageToAction :: InMessage -> Action
-messageToAction (InTextMessage text) = case text of
-  "/help"   -> Help
-  otherwise -> Echo text
+messageToAction message = case message of
+  InTextMessage text ->
+    case text of
+      "/help"   -> Help
+      "/repeat" -> RequestRepeatKeyboard
+      otherwise -> Echo text
+  KeyboardKeyPushed n -> SetRepeatTo n
 
-react :: Config -> InMessage -> [OutMessage]
+react :: Config -> InMessage -> (Config, [OutMessage])
 react config message
   = case messageToAction message of
-      Echo text -> replicate (repeats config) $ OutTextMessage text
-      Help -> [OutTextMessage $ helpText config]
+      Echo text ->
+        (config, replicate (repeats config) $ OutTextMessage text)
+      Help ->
+        (config, [OutTextMessage $ helpText config])
+      RequestRepeatKeyboard ->
+        (config, [SendKeyboard (repeatKeyboardText config) [1, 2, 3, 4, 5]])
+      SetRepeatTo n ->
+        (config { repeats = n }, [OutTextMessage $ "The messages will now be repeated " ++ show n ++ " times."])
--- test/Spec.hs
+++ test/Spec.hs
@@ -8,14 +8,22 @@ spec = do
   describe "Bot.react" $ do
     it "echoes any non-command message n times, n is configurable" $ do
       react (defaultConfig { repeats = 1 }) (InTextMessage "text")
-        `shouldBe` [OutTextMessage "text"]
+        `shouldBe` (defaultConfig { repeats = 1 }, [OutTextMessage "text"])
       react (defaultConfig { repeats = 2 }) (InTextMessage "text")
-        `shouldBe` [OutTextMessage "text", OutTextMessage "text"]
+        `shouldBe` (defaultConfig { repeats = 2 }, [OutTextMessage "text", OutTextMessage "text"])
     it "replies with the help message when it receives /help command" $ do
       react defaultConfig (InTextMessage "/help")
-        `shouldBe` [OutTextMessage (helpText defaultConfig)]
+        `shouldBe` (defaultConfig, [OutTextMessage (helpText defaultConfig)])
+    it "sends the repeat keyboard when it receives /repeat command" $ do
+      react defaultConfig (InTextMessage "/repeat")
+        `shouldBe` (defaultConfig, [SendKeyboard (repeatKeyboardText defaultConfig) [1, 2, 3, 4, 5]])
+    it "sets the number of repeats when the user pushes a button on the keyboard" $ do
+      react defaultConfig (KeyboardKeyPushed 5)
+        `shouldBe` (defaultConfig { repeats = 5 }, [OutTextMessage "The messages will now be repeated 5 times."])
 
   describe "Bot.messageToAction" $ do
     it "separates commands and regular messages" $ do
       messageToAction (InTextMessage "Some text") `shouldBe` Echo "Some text"
       messageToAction (InTextMessage "/help") `shouldBe` Help
+      messageToAction (InTextMessage "/repeat") `shouldBe` RequestRepeatKeyboard
+      messageToAction (KeyboardKeyPushed 1) `shouldBe` SetRepeatTo 1
