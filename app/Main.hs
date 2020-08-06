ff907a1a43fac96088da3439c30754e76a29dbbf
Gleb Akhmerov <g.akhmerov@gmail.com>
2020-08-06 20:00:50 +0400

    Add config file parsing (closes #8)

--- app/Main.hs
+++ app/Main.hs
@@ -1,14 +1,23 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
 import Control.Monad.State (StateT, evalStateT, liftIO)
+import qualified Data.Text.IO as T
+import Data.Ini.Config (IniParser, section, fieldOf, string, parseIniFile)
 import Bot (react, InMessage(..), OutMessage(..), defaultConfig)
 
-loop :: StateT Int IO ()
-loop = do
-  inText <- liftIO getLine
-  SendMessageTimes n outText <- react defaultConfig (InTextMessage inText)
-  liftIO $ mapM_ putStrLn (replicate n outText)
-  loop
+data AuthConfig = AuthConfig
+  { telegramToken :: String }
+  deriving Show
+
+configParser :: IniParser AuthConfig
+configParser = do
+  section "Telegram" $ do
+    tgToken <- fieldOf "token" string
+    return AuthConfig { telegramToken = tgToken }
 
 main :: IO ()
-main = evalStateT loop 2
+main = do
+  configText <- T.readFile "config.ini"
+  print $ parseIniFile configText configParser
