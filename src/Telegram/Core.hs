557e4a6ee3060c8f507bf0100f48e0d2252b54f9
Gleb Akhmerov <g.akhmerov@gmail.com>
2020-08-23 15:11:03 +0400

    Clean up

--- src/Telegram/Core.hs
+++ src/Telegram/Core.hs
@@ -9,7 +9,6 @@ import Control.Monad.State ( StateT, liftIO )
 import Bot
 import Telegram.BotTypes
 import Telegram.Api
-import Prelude hiding ( id )
 
 sendOutMessage :: Token -> UserId -> OutMessage MessageId -> IO ()
 sendOutMessage token userId outMessage =
@@ -21,39 +20,41 @@ sendOutMessage token userId outMessage =
     SendKeyboard text buttons ->
       sendKeyboard token userId text buttons
 
-handleUpdate :: Token -> Update -> Config -> StateT Int IO ()
-handleUpdate _ (u @ UnknownUpdate {}) _ =
-  liftIO $ putStrLn $ "Ignoring update: " ++ show u
-handleUpdate token (Update { uUserId, uEvent }) config =
-  case uEvent of
-    EventMessage (MessageWithId id message) -> do
-      outMessage <- case message of
-        TextMessage { tmText } ->
-          react config (InTextMessage tmText id)
-        MediaMessage ->
-          react config (InMediaMessage id)
-      liftIO $ sendOutMessage token uUserId outMessage
-    CallbackQuery { cqId, cqData } -> do
-      outMessage <- react config (KeyboardKeyPushed cqData)
-      liftIO $ sendOutMessage token uUserId outMessage
-      liftIO $ answerCallbackQuery token cqId
+handleUpdate :: Config -> Token -> Update -> StateT Int IO ()
+handleUpdate config token update =
+  case update of
+    UnknownUpdate {} ->
+      liftIO $ putStrLn $ "Ignoring update: " ++ show update
+    Update { uUserId, uEvent } ->
+      case uEvent of
+        EventMessage (MessageWithId mesId message) -> do
+          outMessage <- case message of
+            TextMessage { tmText } ->
+              react config (InTextMessage tmText mesId)
+            MediaMessage ->
+              react config (InMediaMessage mesId)
+          liftIO $ sendOutMessage token uUserId outMessage
+        CallbackQuery { cqId, cqData } -> do
+          outMessage <- react config (KeyboardKeyPushed cqData)
+          liftIO $ sendOutMessage token uUserId outMessage
+          liftIO $ answerCallbackQuery token cqId
 
-handleUpdates :: Token -> [Update] -> Config -> StateT Int IO ()
-handleUpdates token updates config =
+handleUpdates :: Config -> Token -> [Update] -> StateT Int IO ()
+handleUpdates config token updates =
   case updates of
     [] -> return ()
     (u:us) -> do
-      handleUpdate token u config
-      handleUpdates token us config
+      handleUpdate  config token u
+      handleUpdates config token us
 
-runBot :: Token -> UpdateId -> Config -> StateT Int IO ()
-runBot token offset config = do
+runBot :: Config -> Token -> UpdateId -> StateT Int IO ()
+runBot config token offset = do
   liftIO $ putStrLn $ "Offset: " ++ show offset
   eitherUpdates <- liftIO $ getUpdates token offset
   case eitherUpdates of
     Left err      -> liftIO $ putStrLn err
-    Right []      -> runBot token offset config
+    Right []      -> runBot config token offset
     Right updates -> do
-      handleUpdates token updates config
+      handleUpdates config token updates
       let newOffset = updates & last & uId & unUpdateId & (+1) & UpdateId
-      runBot token newOffset config
+      runBot config token newOffset
